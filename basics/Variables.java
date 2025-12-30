12832fd08a468734a65864421b07bdc3636c8b4b
alexander-durand-dev <alexanderlopezdurand09@gmail.com>
2025-12-30 18:10:06 -0500

    Add variables and data types example

---
+++ basics/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        int age = 21;
+        double height = 1.73;
+        String name = "Alexander";
+        boolean isStudent = true;
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is student?: " + isStudent);
+    }
+}
